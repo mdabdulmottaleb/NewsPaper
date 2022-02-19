a58973a6d5b5504fdf9cb61e0cd1800ba7b45236
Mottaleb <abdulmottaleb945@gmail.com>
2022-02-19 18:12:18 +0600

    Initial Commit

---
+++ NewsPaper/NewsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPaperApp.swift
+//  NewsPaper
+//
+//  Created by MacBook Pro on 19/2/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
